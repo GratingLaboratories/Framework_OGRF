97cc198009941b9e0219866cd00106e2d4b2bea7
Robert Lu <luwuyue@outlook.com>
2017-06-21 09:46:40 +0800

    rm directory tetra

--- MeshCompression/scene/test/tetra/a.lua
+++
@@ -1,6 +0,0 @@
--- Lua script.
-p=tetview:new()
-p:load_mesh("C:/Users/luwuy/Source/Repos/MeshCompression/MeshCompression/scene/test/tetra/horse.ele")
-rnd=glvCreate(0, 0, 500, 500, "TetView")
-p:plot(rnd)
-glvWait()
